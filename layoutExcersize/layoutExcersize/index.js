b645b241e924522baeb69c9bdd99effdc1622920
mmavliev <46608486+mmavliev@users.noreply.github.com>
2020-01-14 00:05:14 +0300

    Add files via upload

---
+++ layoutExcersize/layoutExcersize/index.js
@@ -0,0 +1,21 @@
+let elements = $(".form .form-value").toArray();
+$.map(elements, function(el) {
+    let rowWidth = $(el).parent("div").width();
+    let elWidth = $(el).parent("div").find(".form-field").width();
+    $(el).css("width", rowWidth - elWidth - 14)
+})
+$(".checkboxes .form-field").click(function() {
+    let img = $(this).find("img");
+    if (img.attr("src") == "assets/check.png") {
+        img.attr("src", "assets/uncheck.png")
+    } else {
+        img.attr("src", "assets/check.png")
+    }
+})
+$(".javascript").click(function(event) {
+    let padding = $(".javascript").offset().left;
+    let x = event.clientX - padding;
+    $(".polz").css("margin-left", x);
+
+})
+$(".date").css("padding-bottom", $(document).height() - $(".date").offset().top - $(".date").height() - 150)
